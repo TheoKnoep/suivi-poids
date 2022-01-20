e05c7c4057ce3f6aa0297c8d44a69937b0bc84b1
Théo Knoepflin <theo.knoepflin@gmail.com>
2022-01-20 19:18:49 +0100

    service worker en network first

--- sw.js
+++ sw.js
@@ -13,21 +13,36 @@ let staticAssets = [
 	"css/style.css"
 ]
  
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(staticAssets);
-    })
-  );
+self.addEventListener("install", function (event) {
+	self.skipWaiting(); 
+	event.waitUntil(
+		caches.open(staticCacheName).then(function (cache) {
+		return cache.addAll(staticAssets);
+		})
+  	);
 });
  
 self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
+//   console.log(event.request.url);
  
   event.respondWith(
-    caches.match(event.request)
-		.then(function (response) {
-			return response || fetch(event.request);
-		})
+    // caches.match(event.request)
+	// 	.then(function (response) {
+	// 		return response || fetch(event.request);
+	// 	})
+	// fetch(event.request)
+	// 	.then(res => {
+	// 		console.log(res.status); 
+	// 		return res; 
+	// 	})
+	// 	.catch(err => {
+	// 		console.log('Meh ', err); 
+	// 	})
+
+	fetch(event.request)
+		.catch(function() {
+		return caches.match(event.request)
+	})
+
   );
 });
