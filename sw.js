d1fda6a17ad3e1dbadcb5ba599219e726f20e8d5
Théo Knoepflin <theo.knoepflin@gmail.com>
2022-01-21 16:29:25 +0100

    SW mise en cache des requetes réussie

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ let staticAssets = [
 	"assets/ico-suivi-poids.png",
 	"assets/icon_48x48.png",
 	"assets/icon_72x72.png",
-	"assets/icon96x96_.png",
+	"assets/icon_96x96.png",
 	"assets/icon_144x144.png",
 	"assets/icon_168x168.png",
 	"assets/icon_192x192.png"
@@ -32,8 +32,11 @@ self.addEventListener("install", function (event) {
 self.addEventListener('fetch', (e) => {
 	e.respondWith(
 		fetch(e.request)
-			.then(res => {
+			.then(async res => {
 				console.log(res.ok); 
+				const cache = await caches.open(staticCacheName); 
+				console.log('[SW] caching new ressource ' + e.request.url); 
+				cache.put(e.request, res.clone());
 				return res; 
 			})
 			.catch(err => {
